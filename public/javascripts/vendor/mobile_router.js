70a9ef58f2715d940c9eb0c86630fee67859c08f
CodeOfficer <codeofficer@gmail.com>
2012-02-07 13:56:38 -0500

    mobile boiler plate hack to speed up links on mobile safari

--- public/javascripts/vendor/mobile_router.js
+++ public/javascripts/vendor/mobile_router.js
@@ -15,6 +15,24 @@ _.extend(Support.MobileRouter.prototype, Backbone.Router.prototype, {
     var page = $(this.currentView.el).page();
     var changePageOptions = {changeHash: false, fromHashChange: false}
 
+    // BEGIN MOBILE BOILER PLATE HACK
+    $('a:not(.ui-collapsible-heading-toggle, .no-fast-button)', this.currentView.el).each(function() {
+      var $self = $(this);
+      new MBP.fastButton(this, function() {
+        window.location.hash = $self.attr('href');
+
+        if ($self.attr('data-direction') !== undefined) {
+          $("div[data-role='page']").last().attr("data-direction", $self.attr('data-direction'));
+        };
+        if ($self.attr('data-transition') !== undefined) {
+          $("div[data-role='page']").last().attr("data-transition", $self.attr('data-transition'));
+        };
+
+        return false;
+      });
+    });
+    // END MOBILE BOILER PLATE HACK
+
     if (oldView && $(oldView.el).attr("data-direction") === 'reverse') {
       changePageOptions["reverse"] = true;
     };
